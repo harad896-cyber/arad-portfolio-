4fb0e18e2575434e97929e75eaed2b6b0917b89e
harad896-cyber <290895648+harad896-cyber@users.noreply.github.com>
2026-09-16 10:38:16 -0700

    test: mount ProfileGate inside MaterialApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:arad_messenger/main.dart';
 
 void main() {
   testWidgets('app starts', (tester) async {
-    await tester.pumpWidget(const ProfileGate());
-    await tester.pumpAndSettle();
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: ProfileGate(),
+      ),
+    );
+    await tester.pump();
     expect(find.byType(ProfileGate), findsOneWidget);
   });
 }
